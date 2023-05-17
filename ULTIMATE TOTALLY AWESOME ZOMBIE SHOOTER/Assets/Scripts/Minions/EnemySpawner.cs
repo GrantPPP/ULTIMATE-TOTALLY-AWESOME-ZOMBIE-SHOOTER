972ae3d8620ed6da67b3d2ac995f5e37c4f00021
GrantPPP <111304364+GrantPPP@users.noreply.github.com>
2023-05-17 10:57:33 -0700

    background, bug fixes

--- ULTIMATE TOTALLY AWESOME ZOMBIE SHOOTER/Assets/Scripts/Minions/EnemySpawner.cs
+++ ULTIMATE TOTALLY AWESOME ZOMBIE SHOOTER/Assets/Scripts/Minions/EnemySpawner.cs
@@ -56,6 +56,8 @@ public class EnemySpawner : MonoBehaviour
         boss = GameObject.FindWithTag("Boss"); 
         
         stop = false;
+
+        spawnTime = 0;
     }
 
     // Update is called once per frame
